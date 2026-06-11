62fa0424225b1249fdbe95e4b7aa3c4855e972e7
julianoCoelho <mwgmaker@gmail.com>
2026-06-11 17:54:04 -0300

    Pagina de Erro -NotFound

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,17 +1,19 @@
 import { useNavigate } from 'react-router-dom';
-import { FiHome, FiAlertTriangle } from 'react-icons/fi';
+import { FiHome} from 'react-icons/fi';
+import { FaGhost } from "react-icons/fa";
 import styles from './styles.module.css';
 
 export default function NotFound() {
   const navigate = useNavigate();
   return (
     <div className={styles.page}>
-      <div className={styles.icon}><FiAlertTriangle /></div>
+      <div className={styles.iconArea}>
+      <FaGhost size={80} className={styles.bounceIcon} />
+      </div>
       <h1 className={styles.code}>404</h1>
-      <p className={styles.message}>Página não encontrada</p>
-      <p className={styles.sub}>O endereço que você acessou não existe ou foi removido.</p>
-      <button className="btn btn-primary" onClick={() => navigate('/')}>
-        <FiHome /> Voltar ao início
+      <p className={styles.message}>Ops... a página não foi encontrada</p>
+         <button className="btn btn-primary" onClick={() => navigate('/')}>
+        <FiHome /> Voltar ao catalogo de produtos
       </button>
     </div>
   );
