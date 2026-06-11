28504493a6117f9b18e7a2a9dbcd78cd7c80ff67
MatheusSilveira <matheus.santos@residente.serratec.org>
2026-06-11 12:05:35 -0300

    Atualização código do projeto

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://fakestoreapi.com',
+  timeout: 10000,
+});
+
+export const getProducts = () => api.get('/products');
+export const getProduct = (id) => api.get(`/products/${id}`);
+export const getProductsByCategory = (category) => api.get(`/products/category/${category}`);
+export const getCategories = () => api.get('/products/categories');
+export const createProduct = (data) => api.post('/products', data);
+export const updateProduct = (id, data) => api.put(`/products/${id}`, data);
+export const deleteProduct = (id) => api.delete(`/products/${id}`);
+
+export const loginUser = (credentials) => api.post('/auth/login', credentials);
+
+export const getCarts = () => api.get('/carts');
+export const getCart = (id) => api.get(`/carts/${id}`);
+
+export default api;
