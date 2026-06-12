4817649984a19a12d7018c30baf81259c766debf
julianoCoelho <mwgmaker@gmail.com>
2026-06-11 22:44:22 -0300

    Correção no componente Button Genérico (propres)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,9 @@
 import styles from "./styles.module.css";
 // Componente de botão genérico para uso com propries
-export default function Button({ children, onClick, variant = "default" }) {
+export default function Button({ children, onClick, variant = "default", ...props }) {
   return (
-    <button className={`${styles.btn} ${styles[variant]}`} onClick={onClick}>
+    <button className={`${styles.btn} ${styles[variant]}`} onClick={onClick} {...props}>
       {children}
     </button>
   );
 }
-  
